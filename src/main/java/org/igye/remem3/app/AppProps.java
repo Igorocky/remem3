7563cde49077d1daae81a3d5b5e4d9d485d8f3c6
Igorocky <Igorocky@mail>
2025-10-28 19:59:14 +0100

    Implement AppProps.java.

---
+++ src/main/java/org/igye/remem3/app/AppProps.java
@@ -0,0 +1,19 @@
+package org.igye.remem3.app;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+
+@ConfigurationProperties("app")
+@Getter
+@RequiredArgsConstructor
+public class AppProps {
+    private final List<String> languages;
+    private final List<String> directoriesWithCards;
+    private final String cacheFile;
+    private final String cardEditor;
+    private final String bucketDelays;
+    private final List<String> exercises;
+}
--- src/main/java/org/igye/remem3/app/MyApplication.java
+++ src/main/java/org/igye/remem3/app/MyApplication.java
@@ -2,11 +2,13 @@ package org.igye.remem3.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@EnableConfigurationProperties(AppProps.class)
 public class MyApplication {
 
     @RequestMapping("/")
