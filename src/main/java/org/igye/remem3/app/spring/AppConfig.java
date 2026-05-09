3ea5cbccf81555b1fd5872da01effc5839a10f05
Igorocky <Igorocky@mail>
2026-05-09 13:31:37 +0200

    Move everything from AppConfig2 to AppConfig and remove AppConfig2.

--- src/main/java/org/igye/remem3/app/spring/AppConfig.java
+++ src/main/java/org/igye/remem3/app/spring/AppConfig.java
@@ -8,10 +8,19 @@ import org.igye.remem3.app.controllers.exercise.ExerciseController;
 import org.igye.remem3.app.controllers.movecardstodir.MoveCardsToDirController;
 import org.igye.remem3.app.controllers.newcard.NewCardController;
 import org.igye.remem3.app.controllers.validatecards.ValidateCardsController;
+import org.igye.remem3.app.controllers2.beans.BeansConfig;
 import org.igye.remem3.app.controllers2.beans.CustomBeansConfig;
+import org.igye.remem3.app.controllers2.exercise.ExerciseConfig;
+import org.igye.remem3.app.controllers2.index.IndexConfig;
+import org.igye.remem3.app.controllers2.newcard.NewCardConfig;
 import org.igye.remem3.app.impl.CacheImpl;
 import org.igye.remem3.app.impl.CardUtilsImpl;
 import org.igye.remem3.app.impl.SettingsImpl;
+import org.igye.remem3.app.state.StateConstructor;
+import org.igye.remem3.app.state.StateRenderer;
+import org.igye.remem3.app.state.StateRepository;
+import org.igye.remem3.app.state.StateUpdater;
+import org.igye.remem3.app.state.impl.StateRepositoryImpl;
 import org.igye.remem3.utils.Utils;
 import org.igye.remem3.utils.impl.UtilsImpl;
 import org.igye.remem3.web.DispatcherController;
@@ -21,10 +30,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 import tools.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.time.Clock;
+import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -45,6 +56,7 @@ import java.util.stream.Collectors;
         MoveCardsToDirController.class,
     })
 )
+@Import({IndexConfig.class, NewCardConfig.class, BeansConfig.class, ExerciseConfig.class})
 public class AppConfig {
 
     @Bean
@@ -65,6 +77,16 @@ public class AppConfig {
     @Bean
     public Cache cache(Settings settings, Utils utils) {
         return new CacheImpl(utils, new File(settings.getCacheFile()));
+    }
+
+    @Bean
+    public StateRepository stateRepository(
+        Clock clock,
+        List<StateConstructor<?>> stateConstructors,
+        List<StateUpdater<?>> stateUpdaters,
+        List<StateRenderer<?>> stateRenderers
+    ) {
+        return new StateRepositoryImpl(clock, Duration.ofHours(1), stateConstructors, stateUpdaters, stateRenderers);
     }
 
     @Bean
